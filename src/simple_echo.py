97875c8527bee9a5cae2f150b817ac01d480a62b
Felipe Costa <felipeiasi@gmail.com>
2019-04-06 19:22:35 -0300

    Updates

--- src/simple_echo.py
+++ src/simple_echo.py
@@ -26,6 +26,7 @@ def main():
     # we get an "Unauthorized" exception.
     try:
         update_id = bot.get_updates()[0].update_id
+        print(update_id)
     except IndexError:
         update_id = None
 
